bba0fe7900b114df473d9c6302859e68e4a1be20
BuildTools <unconfigured@null.spigotmc.org>
2022-09-19 13:17:30 -0400

    Added slint rendering engine

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,7 @@ struct Node {
 }
 
 fn main() {
-    let mut read_in = String::new();
-
-    io::stdin()
-    .read_line(&mut read_in)
-    .expect("You fucked up.");
-
-    println!("{read_in}");
+    let nodes: [Node; 3];
+    
+    println!("Yoooo");
 }
