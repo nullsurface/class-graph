20242652655910f197c45663eab5df6c82baffd6
Andrew <nullsurface@github.com>
2022-09-24 19:24:50 -0700

    Added triangle

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,43 @@
 #[macro_use]
 extern crate glium;
 
-use glium::glutin;
+use glium::{glutin, Surface};
+
+struct Vertex {
+    position: [f32; 3]
+}
 
 fn main() {
-let mut event_loop = glutin::event_loop::EventLoop::new();
-let window_builder = glutin::window::WindowBuilder::new();
-let context_builder = glutin::ContextBuilder::new();
-let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
+    let mut event_loop = glutin::event_loop::EventLoop::new();
+    let window_builder = glutin::window::WindowBuilder::new();
+    let context_builder = glutin::ContextBuilder::new();
+    let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
 
-event_loop.run(move |ev, _, control_flow| {
-    let next_frame_time = std::time::Instant::now() +
-        std::time::Duration::from_nanos(16_666_667);
-    *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-    match ev {
-        glutin::event::Event::WindowEvent { event, .. } => match event {
-            glutin::event::WindowEvent::CloseRequested => {
-                *control_flow = glutin::event_loop::ControlFlow::Exit;
-                return;
+    // Vertex's for a simple triangle
+    let vert1 = Vertex { position: [-0.5, -0.5];
+    let vert2 = Vertex { position: [0.0, 0.5];
+    let vert3 = Vertex { position: [0.5, -0.25];
+    let shape = vec![vert1, vert2, vert3];
+    
+    
+    // Main Loop for GUI Window
+    event_loop.run(move |ev, _, control_flow| {
+        let mut frame = display.draw();
+        frame.clear_color(0.0, 0.0, 1.0, 1.0);
+        frame.finish().unwrap();
+
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
             },
-            _ => return,
-        },
-        _ => (),
-    }
-});
+            _ => (),
+        }
+    });
 }
